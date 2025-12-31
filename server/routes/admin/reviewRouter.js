78dc5c34f0f988a37d4c0822f1cfdecd699475e6
2mushr00m <sally1196420@gmail.com>
2025-12-31 14:08:03 +0900

    🐛 리뷰 숨기기, 보이기 Param 오타 수정

--- server/routes/admin/reviewRouter.js
+++ server/routes/admin/reviewRouter.js
@@ -5,7 +5,7 @@ import * as reviewController from '../../controllers/reviewController.js';
 const router = express.Router();
 
 router.get('/', reviewController.adminList);
-router.patch('/:revieId/hide', reviewController.adminHide);
-router.patch('/:revieId/show', reviewController.adminShow);
+router.patch('/:reviewId/hide', reviewController.adminHide);
+router.patch('/:reviewId/show', reviewController.adminShow);
 
 export default router;
