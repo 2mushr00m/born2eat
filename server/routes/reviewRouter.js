05dfde333063367c09cb6409e33c2b20c68b0372
2mushr00m <sally1196420@gmail.com>
2026-01-09 17:54:58 +0900

    ✨ 리뷰 수정 기능

--- server/routes/reviewRouter.js
+++ server/routes/reviewRouter.js
@@ -2,10 +2,11 @@
 import express from 'express';
 import * as reviewController from '../controllers/reviewController.js';
 import { blockSuspendedUser } from '../middleware/auth.js';
+import { uploaders } from '../middleware/upload.js';
 
 const router = express.Router();
 
-router.patch('/:reviewId', blockSuspendedUser, reviewController.update);
+router.patch('/:reviewId', blockSuspendedUser, uploaders.review, reviewController.update);
 router.delete('/:reviewId', reviewController.destroy);
 
 router.post('/:reviewId/like', reviewController.like);
